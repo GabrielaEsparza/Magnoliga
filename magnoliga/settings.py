0519dfa9e8d7e10b14b4d7dd3333f10879e8d73a
GabrielaEsparza1 <gerspaguetti1@gmail.com>
2026-04-23 10:18:04 -0600

    forzar el deploy

--- magnoliga/settings.py
+++ magnoliga/settings.py
@@ -104,3 +104,4 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost')]
+
